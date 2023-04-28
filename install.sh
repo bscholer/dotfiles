dfc08c4da7dd42c083a35ed04cc2a44d447d58b9
Ben Scholer <github@benscholer.com>
2023-04-28 13:45:15 -0400

    maybe a quick fix for lazygit?

--- install.sh
+++ install.sh
@@ -242,7 +242,7 @@ install_colorls() {
 install_lazygit() {
   _process "→ Installing lazygit"
 
-  case $package_manager in
+  case "$package_manager" in
     apt-get)
       LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
       curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -452,7 +452,7 @@ download_dotfiles() {
   [[ $? ]] && _success "Repository downloaded"
 }
 
-Link_dotfiles() {
+link_dotfiles() {
   # symlink files to the HOME directory.
   if [[ -f "${DIR}/opt/files" ]]; then
     _process "→ Symlinking dotfiles in /configs"
