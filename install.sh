cd53c3e2dddd829dec77bd660a194ae52934cd68
Ben Scholer <github@benscholer.com>
2023-04-28 13:36:52 -0400

    quick fix for apt

--- install.sh
+++ install.sh
@@ -243,7 +243,7 @@ install_lazygit() {
   _process "→ Installing lazygit"
 
   case $package_manager in
-    apt)
+    apt-get)
       LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
       curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
       tar xf lazygit.tar.gz lazygit
