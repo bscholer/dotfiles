a71f2232da85909bae7d168ed91bf0edeafad83f
Ben Scholer <ben.scholer@dronedeploy.com>
2025-01-30 11:10:29 -0500

    gitlink

--- configs/nvchad/plugins.lua
+++ configs/nvchad/plugins.lua
@@ -81,6 +81,15 @@ local plugins = {
     end,
   },
   {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
